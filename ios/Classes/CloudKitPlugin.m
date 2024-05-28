810f720156aed05d55a2b92beccc23a00d66c718
ZeusMacOSs <nicolastinkl@gmail.com>
2024-05-28 12:15:30 +0800

    Update CloudKitPlugin.m
    #import "CloudKitPlugin.h"
    #if __has_include(<flutter_cloudkit/flutter_cloudkit-Swift.h>)
    #import <flutter_cloudkit/flutter_cloudkit-Swift.h>
    #else
    // Support project import fallback if the generated compatibility header
    // is not copied when this plugin is created as a library.
    // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
    #import "flutter_cloudkit-Swift.h"
    #endif
    
    @implementation CloudKitPlugin
    + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
      [SwiftCloudKitPlugin registerWithRegistrar:registrar];
    }
    @end

--- ios/Classes/CloudKitPlugin.m
+++ ios/Classes/CloudKitPlugin.m
@@ -1,12 +1,19 @@
 #import "CloudKitPlugin.h"
-#if __has_include(<cloud_kit/cloud_kit-Swift.h>)
-#import <cloud_kit/cloud_kit-Swift.h>
+#if __has_include(<flutter_cloudkit/flutter_cloudkit-Swift.h>)
+#import <flutter_cloudkit/flutter_cloudkit-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "cloud_kit-Swift.h"
+#import "flutter_cloudkit-Swift.h"
 #endif
+
+@implementation CloudKitPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftCloudKitPlugin registerWithRegistrar:registrar];
+}
+@end
+
 
 @implementation CloudKitPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
